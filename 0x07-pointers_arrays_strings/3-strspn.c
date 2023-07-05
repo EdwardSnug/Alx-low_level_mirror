ad3eb9b25877a27d877d93379de399a5f7b41c62
EdwardSnug <prezokams@gmail.com>
2023-07-05 06:14:03 -0700

    0x08. C - Recursion

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  *_strspn - A function that gets the length of string
+  * (s)
+unsigned int _strspn(char *s, char *accept)
+{
+	int length, i;
+
+	length = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		char *c = accept;
+		while (*c)
+		{
+			if (s[i] == *c)
+			{
+				length = length + 1;
+				break;
+			}
+			else
+			{
+				return length;
+			}
+			i++;
+		}
+	}
+	return (length);
+}
