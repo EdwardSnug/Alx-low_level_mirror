d15b6dae9f335d2832d30e45ba6ad1a14b75ea52
EdwardSnug <prezokams@gmail.com>
2023-07-09 16:01:21 -0700

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,33 @@
 #include "main.h"
 /**
+ *_sqrt - A helper recursive square root function
+ *
+ *@n: Integer number to look for natural squares
+ *@i: An iterator
+ *
+ *Return: returns sqrt function
+ */
+int _sqrt(int n, int i)
+{
+	int square = i * i;
+
+	if (square > n)
+	{
+		return (-1);
+	}
+	else if (square == n)
+	{
+		return (i);
+	}
+	return (_sqrt(n, i + 1));
+}
+/**
   *_sqrt_recursion - A function that calculates the natural
   * square root of a number.
   *
   *@n: Number to look for the natual square root
   *
-  *Return: returns the natural square root or -1
+  *Return: returns(0) on success or -1 to show any error
   */
 int _sqrt_recursion(int n)
 {
@@ -13,27 +35,9 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	return (_sqrt(n, 0));
-	/**
-	  *_sqrt - A helper recursive square root function
-	  *
-	  *@n Integer number to look for natural squares 
-	  *@i: An iterator
-	  *
-	  *@Return: returns sqrt function
-	  */
-	int _sqrt(int n, int i)
+	else
 	{
-		int square = i * i;
-
-		if (square > n)
-		{	
-			return (-1);
-		}
-		if (square == n)
-		{
-			return (i);
-		}
-		return (_sqrt(n, 1 + 1));
+		return (_sqrt(n, 1));
 	}
+	return (0);
 }
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,4 @@
+#include "main.h"
+/**
+  *is_prime_number - A function that checks if an integer is a prime number
+
