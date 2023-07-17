d9d0c8f410f49ce903d52967fbdda268e966a1cd
EdwardSnug <prezokams@gmail.com>
2023-07-17 09:51:04 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  *struct dog - A record/structure consisting of different variables
+  *
+  *@name: Name of our given dog
+  *@age: The age of the dog
+  *@owner: The caretaker/guardian of the dog
+  *
+  *Description: A record data structure about a dog and
+  *its different characteristics
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
--- README.md
+++ README.md
@@ -12,3 +12,4 @@
 #0x0B. C - malloc, free
 #0x0C. C - More malloc, free
 #0x0D. C - Preprocessor
+#0x0E. C - Structures, typedef
