6f66614779961d62e2cd39de90cc1aebbe530514
EdwardSnug <prezokams@gmail.com>
2023-07-28 12:10:41 -0700

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/hello_holberton.c
+++ 0x12-singly_linked_lists/hello_holberton.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main()
+/**
+  *main - Entry for our program
+  *
+  *Return: returns zero always on success
+  */
+int main(void)
 {
 	printf("Hello, Holberton\n");
+	return (0);
 }
