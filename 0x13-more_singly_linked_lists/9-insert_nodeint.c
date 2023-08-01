828f0156973740453849c26a4fac5597d63eb8ec
EdwardSnug <prezokams@gmail.com>
2023-08-01 15:32:36 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,7 +39,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 	}
 	/**an index out of bounds*/
-	if (i < idx)
+	if (i > idx)
 	{
 		free(new_node);
 		return (NULL);
