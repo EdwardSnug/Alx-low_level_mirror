b0904e09afb3cce06e84b7b171f6d1a5d35a19b0
EdwardSnug <prezokams@gmail.com>
2023-08-07 17:39:38 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,15 +22,12 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content != NULL)
+	byteswritten = write(fd, text_content, str_length);
+	if (byteswritten == -1)
 	{
-		byteswritten = write(fd, text_content, str_length);
-		if (byteswritten == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+		close(fd);
+		return (-1);
 	}
 	close(fd);
-	return (-1);
+	return (1);
 }
